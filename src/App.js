6f77222271d4cc3907acda127da7efc379b76a11
Pearltee28 <pearlngubane127@gmail.com>
2024-10-04 18:26:52 +0200

    Added search input and some css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import Weather from "./weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Pearl Ngubane and is{" "}
-        <a
-          href="https://github.com/Pearltee28/react-weather-homework.git"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was coded by Pearl Ngubane and is{" "}
+          <a
+            href="https://github.com/Pearltee28/react-weather-homework.git"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
